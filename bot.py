0fed1f95ee178e6e87f959e382b6e91459479747
Ramat554 <51392170+Ramat554@users.noreply.github.com>
2019-10-02 10:25:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,50 @@
-"""Flask App Project."""
+from flask import Flask, jsonify, request
+import os
+import json
+import requests
 
-from flask import Flask, jsonify
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-    """Return homepage."""
-#    json_data = {'Hello': 'World!'}
-#    return jsonify(json_data)                                                                               
-    return "นายราเมศ อยู่เจริญ เลข1 ห้อง 4/1"
+    a=os.environ['Authorization']
+    return "นายราเมศ อยู่เจริญ เลขที่ 1 ชั้น ม.4/1"
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        return "OK"
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded['originalDetectIntenRequest']['payload']['data']['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']
+    #sendText(user,userText)
+    If (userText == 'สวัสดี') :
+        sendText(user,'สวัสดีจ้า')
+    elif(userText == 'ขอตังหน่อย') :
+        sendText(user,'ไม่ให้จ้า')
+     elif(userText == 'เสียใจนะ') :
+        sendText(user, 'เราไม่มีเงิน แบร่!!') 
+     elif :
+        sendText(user, 'ไม่เข้าใจ') 
+        
+    return '',200
+
+def sendText(user, text):
+  LINE_API = 'https://api.line.me/v2/bot/message/reply'
+  headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Authorization': os.environ['Authorization']    # ตั้ง Config vars ใน heroku พร้อมค่า Access token
+  }
+  data = json.dumps({
+    "replyToken":user,
+    "messages":[{"type":"text","text":text}]
+  })
+  r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
 
 if __name__ == '__main__':
     app.run()
