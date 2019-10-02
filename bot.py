4846d1a16be216a0627b08ac2f7911cafe118527
Ramat554 <51392170+Ramat554@users.noreply.github.com>
2019-10-02 10:26:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,9 +27,9 @@ def callback():
         sendText(user,'สวัสดีจ้า')
     elif(userText == 'ขอตังหน่อย') :
         sendText(user,'ไม่ให้จ้า')
-     elif(userText == 'เสียใจนะ') :
+    elif(userText == 'เสียใจนะ') :
         sendText(user, 'เราไม่มีเงิน แบร่!!') 
-     elif :
+    elif :
         sendText(user, 'ไม่เข้าใจ') 
         
     return '',200
